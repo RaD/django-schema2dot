7628cdad65ac1d601d6ad978e5a39459e2f84025
Ruslan Popov <ruslan.popov@gmail.com>
2010-12-07 11:25:14 +0300

    Ugly typo.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name='schema2dot',
       version='0.2',
-      description='Django application that can export project's database schema into .dot format of Graphviz.',
+      description='Django application that can export project\'s database schema into .dot format of Graphviz.',
       author='Ruslan Popov',
       author_email='ruslan.popov@gmail.com',
       url='http://github.com/RaD/django-schema2dot/tree/master',
